78f247bf66f8525952e9cafdc47678306b68836d
Ashley Radford <aradford7@outlook.com>
2019-04-11 17:40:01 -0700

    commit3

--- server.js
+++ server.js
@@ -8,6 +8,23 @@ app.get('/tip/:total/:tipPercentage', (request,response) => {
 
 
 
+const MagicB = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+
+//localhost:3000/magic/Will%20I%20Be%20A%20Millionare
+app.get('/magicb/:thisishard',(request,response) => {
+    response.send(`${MagicB[Math.floor(Math.random()*20)]}`); //20 is the max so 1*20 to give value
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
