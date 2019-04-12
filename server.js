f1213ff076b75750ea91d951bdc189a197089830
Ashley Radford <aradford7@outlook.com>
2019-04-11 17:25:12 -0700

    Commit2

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express'); //from documentation: express is function
 const app = express();//app is an object
 
+app.get('/tip/:total/:tipPercentage', (request,response) => {
+    response.send(`${request.params.total*request.params.tipPercentage/100}`);
+})
+//in localhost:3000/tip/100/20
+
+
+
+
+
+
 
 
 app.get('/greeting/:name', (request, response) => {
     
     response.send(`Hello! ${request.params.name}`);
-});  //localhost:3000/greeting/name
+});  //localhost:3000/greeting/Ashley
+            //shows Hello Ashley
 // app.get('/greeting/:name', (req, res) => {
 //     res.send({params: req.params, queries: req.query})
 //   });
